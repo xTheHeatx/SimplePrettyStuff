71cc638040a074f9c339300780b1459c72709c8d
Medicino <105019072+xTheHeatx@users.noreply.github.com>
2022-10-24 20:58:27 +0400

    !

--- js/parallax.js
+++ js/parallax.js
@@ -9,17 +9,25 @@ const scrollEvent = document.addEventListener('scroll', e => {
 
 
 
-    slider1.setAttribute("style", `transform: translateY(calc(-${shortNumber}px + 2px));`)
+    slider1.setAttribute("style", `background-position-y: calc(-${shortNumber}px + 2px);`)
 
 
 
     // slider2.setAttribute("style", `background-position-y: calc(-${shortNumber}px + 2px); transform: translateY(calc(-${shortNumber}px + 2px));`)
-    slider2.setAttribute("style", `transform: translateY(calc(-${shortNumber}px + 2px));`)
+    // slider2.setAttribute("style", `background-position-y: calc(-${shortNumber}px + 6px);`)
 
 
 
 })
 
+
+let slider1 = document.querySelector('.main-container')
+
+function pos(e) {
+    slider1.setAttribute("style", `background-position-x: calc(-${e.pageX}px / 3); background-position-y: calc(-${e.pageY}px / 2);`)
+}
+
+addEventListener('mousemove', pos, false);
 
 
 
