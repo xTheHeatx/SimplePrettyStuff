7dd30eb051ae7ead60c51265689e596459526d51
Medicino <105019072+xTheHeatx@users.noreply.github.com>
2022-10-25 18:00:48 +0400

    !

--- js/main.js
+++ js/main.js
@@ -5,3 +5,20 @@ const sliderMain = new Swiper('.slider_main', {
     mousewheel: false,
     slidesPerView: 2,
 })
+
+
+
+
+// const imgRequest = (url) => {
+//     const res = fetch(url,
+//         {
+//             method: "GET",
+
+//             mode: "no-cors"
+
+//         })
+//         .then(response => console.log(response))
+//         .catch(error => console.log(error))
+// }
+
+// imgRequest('https://drive.google.com/uc?export=view&id=1-tAmDY4L4izaSg8CvB3W8ORWshGgMYUI')
--- js/parallax.js
+++ js/parallax.js
@@ -1,7 +1,6 @@
 const scrollEvent = document.addEventListener('scroll', e => {
 
     const slider1 = document.querySelector('.main-container')
-    // const slider2 = document.querySelector('.slider2')
 
     const scrollPosition = window.scrollY
 
@@ -13,14 +12,7 @@ const scrollEvent = document.addEventListener('scroll', e => {
 
 
 
-    // slider2.setAttribute("style", `background-position-y: calc(-${shortNumber}px + 2px); transform: translateY(calc(-${shortNumber}px + 2px));`)
-    // slider2.setAttribute("style", `background-position-y: calc(-${shortNumber}px + 6px);`)
-
-
-
 })
-
-
 
 
 
